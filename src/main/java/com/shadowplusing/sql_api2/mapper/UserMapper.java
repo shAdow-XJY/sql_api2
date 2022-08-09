8839db5968fb2667ed2cac2a40501d27e26a955e
shAdowplusing <1290232854@qq.com>
2022-08-09 13:26:50 +0800

    mapper添加@Mapper注解

--- src/main/java/com/shadowplusing/sql_api2/mapper/UserMapper.java
+++ src/main/java/com/shadowplusing/sql_api2/mapper/UserMapper.java
@@ -2,9 +2,11 @@ package com.shadowplusing.sql_api2.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.shadowplusing.sql_api2.entity.User;
+import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@Mapper
 public interface UserMapper extends BaseMapper<User> {
 
 }
